b62edc6f8da6ab987d0e9d3ae9d216e8908b1936
KhanhTSN <khanhtsn@younetco.com>
2017-02-15 11:03:45 +0700

    Initial app

---
+++ Install.php
@@ -0,0 +1,34 @@
+<?php
+            namespace Apps\ElasticSearch;
+
+            use Core\App;
+
+            /**
+             * Class Install
+             * @author  Neil
+             * @version 4.5.0
+             * @package Apps\ElasticSearch
+             */
+            class Install extends App\App
+            {
+                private $_app_phrases = [
+        
+                ];
+                protected function setId()
+                {
+                    $this->id = 'ElasticSearch';
+                }
+                protected function setAlias() 
+                {
+            }
+            protected function setName()
+            {
+                $this->name = 'ElasticSearch';
+            } protected function setVersion() {
+                $this->version = '4.1.0';
+            } protected function setSupportVersion() {
+            $this->start_support_version = '4.5.1';
+            $this->end_support_version = '4.5.1';
+        } protected function setSettings() {} protected function setUserGroupSettings() {} protected function setComponent() {} protected function setComponentBlock() {} protected function setPhrase() {
+            $this->phrase = $this->_app_phrases;
+        } protected function setOthers() {}}
